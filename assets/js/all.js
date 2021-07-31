754322dafc64e34bdda5f3d66b334d94dacae426
peter.chen1024 <u0052017@gmail.com>
2021-08-01 00:11:11 +0800

    Update 2021-07-31T16:11:08.897Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,13 +3,19 @@
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!'); // DOM
-  // nav選項
+  // 產品頁
 
-  var optical = $(".banner__list").children()[0];
-  var sunglasses = $(".banner__list").children()[1]; // productList
+  var optical = $(".banner__list").children()[0]; // nav選項
 
-  var opticalList = $(".optical-block")[0];
-  var sunGlassesList = $(".sunglasses-block")[0];
+  var sunglasses = $(".banner__list").children()[1];
+  var opticalList = $(".optical-block")[0]; // productList
+
+  var sunGlassesList = $(".sunglasses-block")[0]; // 據點頁
+
+  var locationList = document.querySelector(".location__list");
+  var locationInformation = document.querySelector(".location__information "); // 事件
+  // 產品頁
+
   $(".banner__list").click(function (e) {
     e.preventDefault(); // $(this).toggleClass("active");
 
@@ -34,6 +40,13 @@ $(document).ready(function () {
       default:
         break;
     }
+  }); // 據點頁
+
+  $(".location-button").click(function (e) {
+    e.preventDefault(); // $(this).toggleClass("active");
+
+    locationList.classList.add('d-none');
+    locationInformation.classList.remove('d-none'); // console.log(locationInformation);
   });
 });
 //# sourceMappingURL=all.js.map
