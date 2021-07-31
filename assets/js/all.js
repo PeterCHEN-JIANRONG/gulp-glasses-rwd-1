de5980d84ea377c8a9c2e6e23614221658c3744e
peter.chen1024 <u0052017@gmail.com>
2021-07-31 20:41:26 +0800

    Update 2021-07-31T12:41:23.801Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,6 +2,38 @@
 
 console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  console.log('HesSchool Hello!'); // DOM
+  // nav選項
+
+  var optical = $(".banner__list").children()[0];
+  var sunglasses = $(".banner__list").children()[1]; // productList
+
+  var opticalList = $(".optical-block")[0];
+  var sunGlassesList = $(".sunglasses-block")[0];
+  $(".banner__list").click(function (e) {
+    e.preventDefault(); // $(this).toggleClass("active");
+
+    var option = e.target.dataset.category || e.target.parentNode.dataset.category;
+
+    switch (option) {
+      case 'optical':
+        // optical.toggleClass("active");
+        optical.classList.add('active');
+        sunglasses.classList.remove('active');
+        opticalList.classList.remove('d-none');
+        sunGlassesList.classList.add('d-none');
+        break;
+
+      case 'sunglasses':
+        sunglasses.classList.add('active');
+        optical.classList.remove('active');
+        sunGlassesList.classList.remove('d-none');
+        opticalList.classList.add('d-none');
+        break;
+
+      default:
+        break;
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
