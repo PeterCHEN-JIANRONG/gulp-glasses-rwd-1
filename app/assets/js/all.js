810206d7ee6c3e09680b1d91b7528bb783438fe9
peter.chen1024 <u0052017@gmail.com>
2021-07-31 23:16:41 +0800

    add 門市列表,樣式

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,13 +3,17 @@ console.log('Hello!');
 $(document).ready(() => {
   console.log('HesSchool Hello!');
   // DOM
-  // nav選項
-  const optical = $(".banner__list").children()[0];
+  // 產品頁
+  const optical = $(".banner__list").children()[0]; // nav選項
   const sunglasses = $(".banner__list").children()[1];
-  // productList
-  const opticalList = $(".optical-block")[0];
+  const opticalList = $(".optical-block")[0]; // productList
   const sunGlassesList = $(".sunglasses-block")[0];
+  // 據點頁
+  const locationList = document.querySelector(".location__list");
+  const locationInformation = document.querySelector(".location__information ");
 
+  // 事件
+  // 產品頁
   $(".banner__list").click(function (e) {
     e.preventDefault();
     // $(this).toggleClass("active");
@@ -32,6 +36,15 @@ $(document).ready(() => {
       default:
         break;
     }
+  });
+
+  // 據點頁
+  $(".location-button").click(function (e) {
+    e.preventDefault();
+    // $(this).toggleClass("active");
+    locationList.classList.add('d-none');
+    locationInformation.classList.remove('d-none');
+    // console.log(locationInformation);
   });
 
 });
